82942ec31f3d0137f486333cb183bc5ff9f18d92
JosueAPF <josueporras124@gmail.com>
2024-11-15 00:17:57 -0600

    actualizacion visual

--- contactos.php
+++ contactos.php
@@ -37,13 +37,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->addAddress($email, $name); // Correo del participante
         $mail->Subject = 'Confirmación de Inscripción';
 
-        $mail->Body = "Hola $name,\n\nGracias por inscribirte en el curso '$nombre_curso'. Aquí están los detalles del curso:\n\n"
+        $mail->Body = "Hola $name,\n\nGracias por inscribirte en el curso '$nombre_curso'. Aqui estan los detalles del curso:\n\n"
                     . "ID del Curso: $id_curso\n"
-                    . "Día: $dia_curso\n"
+                    . "Dia: $dia_curso\n"
                     . "Horario: $horario_curso\n"
                     . "Precio: $precio_curso\n"
-                    . "Descripción: $descripcion_curso\n\n"
-                    . "Esperamos verte pronto!\n\nSaludos,\nEquipo de Inscripciones";
+                    . "Descripcion: $descripcion_curso\n\n"
+                    . "Esperamos verte pronto!\n\nSaludos,\nEquipo de ProgramaYa!";
         
         $mail->send();
         
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->addAddress('josueporras124@gmail.com'); // Correo del administrador
         $mail->Subject = 'Nueva Inscripción en el Curso';
         
-        $mail->Body = "Un nuevo participante se ha inscrito en el curso. Aquí están los detalles:\n\n"
+        $mail->Body = "Un nuevo participante se ha inscrito en el curso. Aqui estan los detalles:\n\n"
                     . "Nombre: $name\n"
                     . "Email: $email\n"
                     . "Curso: $nombre_curso\n"
@@ -68,7 +68,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         $mail->send();
 
-        echo 'Los mensajes fueron enviados correctamente.';
+        //echo 'Los mensajes fueron enviados correctamente.';
+        echo '<div id="message" style="background-color: #28a745; color: white; padding: 10px; border-radius: 5px;">Los mensajes fueron enviados correctamente.</div>';
+        echo '<a href="javascript:history.back()">Regresar</a>';
+        echo '<script>
+            setTimeout(function() {
+                document.getElementById("message").style.display = "none";
+            }, 5000); // desparecera en 5 segundos
+        </script>';
     } catch (Exception $e) {
         echo "Error al enviar el mensaje: {$mail->ErrorInfo}";
     }
